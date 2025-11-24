7f1df3476f5379a807e2a8d3ee014241a4ed2cc4
KTH-4172 <H098S02805@chunchon.gwe.hs.kr>
2025-11-24 13:27:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
 
      labels[0]: {
        "texts": ["리그 오브 레전드는 AOS게임으로서 대한민국에서 2011년에 처음 출시했습니다."],
-       "images": ["https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/c1d412f408112f04dc09d41b8c9186ff1739aa89-1920x1080.jpg?auto=format&fit=fill&q=80&w=1082"],
+       "images": ["https://i.namu.wiki/i/bcJDyma8areiVI20l4oUFYr6Y6LqPw3NczClG_r0PGkmwvFqEzbkdpUkUdIl1b15WotgqANrvYW4p0LcTcXyyA.webp"],
        "videos": [""]
      },
      labels[1]: {
@@ -82,7 +82,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      },
      labels[2]: {
        "texts": ["오버워치는 하이퍼 FPS게임으로서 대한민국에 2016년에 출시되어 2022년에 서비스 종료 후 같은 년도에 오버워치2로 재출시되었습니다."],
-       "images": [""],
+       "images": ["https://static.wikia.nocookie.net/overwatch152/images/b/bf/%EC%98%A4%EB%B2%84%EC%9B%8C%EC%B9%98_%EB%A1%9C%EA%B3%A0.jpg/revision/latest?cb=20160616114533&path-prefix=ko"],
        "videos": [""]
      },
 }
